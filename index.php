b7cdd38d8c246a4a265c09bb7c7c1f8ebec56389
Marwan Mbarki <MarwanMbarki@users.noreply.github.com>
2017-10-13 08:36:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Page Title -->
-    <title>Map Location - COPYRIGHT - MARWAN MBARKI</title>
+    <title>Map Location - COPYRIGHT 2017 - MARWAN MBARKI</title>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="js/bootstrap.js"></script>
